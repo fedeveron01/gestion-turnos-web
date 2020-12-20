33ffd63c8117d9a6fb47cc896c757b294087b41a
veronfederico45 <veronfederico45@gmail.com>
2020-12-20 17:46:23 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -12,9 +12,10 @@ import Calendario from './pantallas/Calendario';
 
 
 
-import { BottomNavigation,BottomNavigationAction,AppBar,Toolbar,makeStyles} from '@material-ui/core';
+import { BottomNavigation,BottomNavigationAction,AppBar,Toolbar,makeStyles,Tab,Tabs} from '@material-ui/core';
 import React,{useState} from 'react';
 
+import SwipeableViews from 'react-swipeable-views';
 import {BrowserRouter as Router, Route,Switch,Redirect,Link} from 'react-router-dom';
 
 function App() {
@@ -22,7 +23,7 @@ function App() {
   var c = 0;
   const abrirAdmin =()=>{
     c+=1;
-    (c)===4?window.location.href="./admin/":console.log()
+    (c)===4?window.location.href="./admin/":console.log("")
   }
   const useStyles = makeStyles({
     root: {
@@ -38,9 +39,17 @@ function App() {
       horizontal: 'center'
     },
   });
+  const handleChange = (event, value) => {
+    setIndex(value);
+  };
+
+  const handleChangeIndex = index => {
+    setIndex(index)
+  };
 
   const classes = useStyles();
-  
+  const [index,setIndex] = useState(0);
+
   return (
 
 
@@ -50,22 +59,24 @@ function App() {
           <AppBar color="secondary" mainClass={classes}>
             <Toolbar position="static" color="red"> 
  
-            &nbsp;<BottomNavigation color="secondary" align="center"
-  value={value}
-  onChange={(event, newValue) => {
-    setValue(newValue);
-  }}
-  showLabels
-  className={classes.root}
->
-<BottomNavigationAction color="red" to="/" component={Link} icon={<img width="40" src="https://i.pinimg.com/originals/88/48/d5/8848d51a04d7f272187ec179f7def64a.png"/>}  />
-  <BottomNavigationAction  color="red" to="/galeria" component={Link} icon={<img width="40" src="https://cdn4.iconfinder.com/data/icons/erp-software-icon-set-1/512/new_order_confirmation_green-512.png"/>}/>
-  <BottomNavigationAction  to="/facebook" component={Link} icon={<img width="40" src="https://cdn.iconscout.com/icon/free/png-256/facebook-social-media-fb-logo-square-44659.png"/>}/>
-  <BottomNavigationAction  onClick={()=>abrirAdmin()} to="/ayuda" component={Link} icon={<img width="35" src="https://image.flaticon.com/icons/png/128/61/61040.png?ga=GA1.2.816560065.1596499200"/>}/>
 
-</BottomNavigation>
+            <div>
+        <Tabs value={index} onChange={handleChange} centered>
+        
+
+        <Tab icon={<img width="40" src="https://i.pinimg.com/originals/88/48/d5/8848d51a04d7f272187ec179f7def64a.png"/>}>   </Tab>
+          <Tab icon={<img width="40" src="https://cdn4.iconfinder.com/data/icons/erp-software-icon-set-1/512/new_order_confirmation_green-512.png"/>} />
+         <Tab icon={<img width="40" src="https://cdn.iconscout.com/icon/free/png-256/facebook-social-media-fb-logo-square-44659.png"/>} />
+         <Tab onClick={()=>abrirAdmin()} icon={<img width="35" src="https://image.flaticon.com/icons/png/128/61/61040.png?ga=GA1.2.816560065.1596499200"/>} />
+
+        </Tabs>
+     
+      </div>
+      
+    
 </Toolbar>
 </AppBar>
+
 </Route> 
       <Route> <Switch>
           <Route path="/admin/yy4jFpDix8xO6CPwhxDEOyz$hIii7HwpmDfp69Iz8sdzQO4CyD02yAAOVjaoeG29ycv8wcOAfJl8lbtNJvVhYgo9wu">
@@ -73,20 +84,16 @@ function App() {
             <Admin />
           </Route>
   
+
+
+
+
           <Route path="/admin">
   
            <Login />
         </Route>
 
-        <Route path="/facebook">
-  
-           <Facebook />
-        </Route>
-
-        <Route path="/ayuda">
-  
-  <Ayuda />
-</Route>
+   
   
       <Route path="/calendario">
   
@@ -94,25 +101,46 @@ function App() {
       </Route>
 
 
-      <Route path="/galeria">
-  
-       <Galeria />
-      </Route>
+    
 
 
 
-          <Route path="/">
-          <Principal />
-
-          </Route>
         </Switch>
         
         
         </Route>
         </Router>
+        <SwipeableViews index={index} onChangeIndex={handleChangeIndex}>
+          <div ><Principal></Principal></div>
+          <div ><Galeria></Galeria></div>
+          <div><Facebook></Facebook></div>
+          <div><Ayuda></Ayuda></div>
 
+        </SwipeableViews> 
 </div>
   );
+
+  
 }
+
+const styles = {
+  tabs: {
+    background: '#fff',
+  },
+  slide: {
+    padding: 15,
+    minHeight: 100,
+    color: '#fff',
+  },
+  slide1: {
+    backgroundColor: '#FEA900',
+  },
+  slide2: {
+    backgroundColor: '#B3DC4A',
+  },
+  slide3: {
+    backgroundColor: '#6AC0FF',
+  },
+};
 
 export default App;
